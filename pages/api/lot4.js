115fa2afa20526479464e4013b8320a2ee85f34d
worachaicho <moopinq@gmail.com>
2026-06-11 15:32:44 +0700

    Update lot4.js

--- pages/api/lot4.js
+++ pages/api/lot4.js
@@ -1,4 +1,3 @@
-const axios = require("axios");
-const XLSX = require("xlsx");
-
-export default async function handler(req, res) {
+export default function handler(req, res) {
+  res.status(200).json({ message: "API OK" });
+}
